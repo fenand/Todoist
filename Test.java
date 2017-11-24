22725aa4ed2cf795af1dcf17962c0af3a2472fcd
Fernando Garcia De Castro <garciaf4c@gmail.com>
2017-11-24 12:23:15 +0100

    Creamos un un test sencillo

---
+++ Test.java
@@ -0,0 +1,33 @@
+
+import java.util.ArrayList;
+
+public class Test
+{
+    /**
+     * Metodo principal
+     */
+    public static void main(String[] args) 
+    {
+     System.out.println("Creamos un objeto todoist...");
+        Todoist todoist = new Todoist();
+        System.out.println("\nMostramos el numero de tareas pendientes...");
+        todoist.mostrarNumeroDeTareasPendientes();
+        System.out.println("\nAñadimos una tarea...");
+        todoist.addTarea("Limpiar la cocina");
+        System.out.println("\nAñadimos otra tarea...");
+        todoist.addTarea("Sacar la basura");
+        System.out.println("\nAñadimos otra tarea...");
+        todoist.addTarea("Dormir la siesta");
+        System.out.println("\nMostramos todas las tareas...");
+        todoist.mostrarTareas();
+        System.out.println("\nEliminamos la segunda tarea...");
+        todoist.eliminarTareaSeleccionada(1);
+        System.out.println("\nMostramos todas las tareas...");
+        todoist.mostrarTareas(); 
+        System.out.println("\nIntentamos borrar una tarea que no existe...");
+        todoist.eliminarTareaSeleccionada(1582);
+        System.out.println("\nMostramos todas las tareas...");
+        
+    }
+
+}
